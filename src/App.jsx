06bc82c7944115feddb515f988acbfedc145ec89
Linda-Gift <lindagift0812@gmail.com>
2024-10-27 14:32:39 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
 
     <ProfileCard />
-    
+
      <ProfileCard 
      image = "./src/assets/settee.jpeg" />
 
@@ -19,6 +19,15 @@ function App() {
      occupation = "Wrestling"
      bio = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos atque enim similique!Eos atque enim similique"
      hobbies = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos atque enim similique!"/>
+
+<ProfileCard 
+     image = "./src/assets/katherine.jpg"
+     username = "Katherine Obadeyi"
+     age = {30}
+     location = "Oklahoma"
+     occupation = "Legal Practitional"
+     bio = "I am a brutal Legal practitional with a keen interest in Litigation with 10 years in experience at the bar with upward interest in Company Law and oil exploration"
+     hobbies = "John Grisham is my go-to book lover. i love indept research, i dance when i am not in the court, i love tasty meals."/>
     </>
   )
 }
